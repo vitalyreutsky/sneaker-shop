fb786b25cbf11b582c5a236ca015fa8c74eb44d7
Vitaly Reutsky <vreutsky77@mail.ru>
2022-04-19 20:36:28 +0300

    Заполнил модалку контентом

--- src/js/components/products.js
+++ src/js/components/products.js
@@ -13,6 +13,16 @@ const catalogMore = document.querySelector(".catalog__more");
 const prodModal = document.querySelector(
   '[data-graph-target="prod-modal"] .modal-content'
 );
+const prodModalSlider = prodModal.querySelector(
+  ".modal-slider .swiper-wrapper"
+);
+const prodModalPreview = prodModal.querySelector(
+  ".modal-slider .modal-preview"
+);
+const prodModalInfo = prodModal.querySelector(".modal-info__wrapper");
+const prodModalDescr = prodModal.querySelector(".modal-prod-descr");
+const prodModalChars = prodModal.querySelector(".prod-chars");
+const prodModalVideo = prodModal.querySelector(".prod-modal__video");
 let prodQuantity = 5;
 let dataLength = null;
 
@@ -71,7 +81,7 @@ if (catalogList) {
                 </div>
               </div>
               <h3 class="product__title">${item.title}</h3>
-              <span class="product__price">${normalPrice(item.price)}</span>
+              <span class="product__price">${normalPrice(item.price)} $</span>
             </article>
           </li>
             `;
@@ -102,11 +112,101 @@ if (catalogList) {
         return response.json();
       })
       .then((data) => {
-        //prodModal.innerHTML = "";
+        prodModalSlider.innerHTML = "";
+        prodModalPreview.innerHTML = "";
+        prodModalInfo.innerHTML = "";
+        prodModalDescr.textContent = "";
+        prodModalChars.innerHTML = "";
+        prodModalVideo.innerHTML = "";
 
-        for (let item of data) {
-          if (item.id == id) {
-            console.log(item);
+        for (let dataItem of data) {
+          if (dataItem.id == id) {
+            console.log(dataItem);
+
+            const slides = dataItem.gallery.map((image) => {
+              return `
+              <div class="swiper-slide">
+                <img src="${image}" alt="">
+              </div>
+              `;
+            });
+
+            const preview = dataItem.gallery.map((image, idx) => {
+              return `
+              <div class="modal-preview__item" data-index="${idx}">
+                <img src="${image}" alt="">
+              </div>
+              `;
+            });
+
+            const sizes = dataItem.sizes.map((size, idx) => {
+              return `
+                 <li class="modal-sizes__item">
+                  <button class="btn-reset modal-sizes__btn">${size}</button>
+                </li>
+              `;
+            });
+
+            prodModalSlider.innerHTML = slides.join("");
+            prodModalPreview.innerHTML = preview.join("");
+
+            prodModalInfo.innerHTML = `
+            <h3 class="modal-info__title">
+              ${dataItem.title}
+            </h3>
+            <div class="modal-info__rate">
+              <svg>
+                <use xlink:href="img/sprite.svg#rate"></use>
+              </svg>
+              <svg>
+                <use xlink:href="img/sprite.svg#rate"></use>
+              </svg>
+              <svg>
+                <use xlink:href="img/sprite.svg#rate"></use>
+              </svg>
+              <svg>
+                <use xlink:href="img/sprite.svg#rate"></use>
+              </svg>
+              <svg>
+                <use xlink:href="img/sprite.svg#rate"></use>
+              </svg>
+            </div>
+            <div class="modal-info__sizes">
+              <span class="modal-info__subtitle">Выберите размер</span>
+              <ul class="list-reset modal-info__sizes-list modal-sizes">${sizes.join(
+                ""
+              )}</ul>
+            </div>
+            <div class="modal-info__price">
+              <span class="modal-info__current-price">${dataItem.price} $</span>
+              <span class="modal-info__old-price">${
+                dataItem.oldPrice ? dataItem.oldPrice + "$" : ""
+              }</span>
+            </div>
+            `;
+
+            prodModalDescr.textContent = dataItem.description;
+
+            let charsItems = "";
+
+            Object.keys(dataItem.chars).forEach(function eachKey(key) {
+              charsItems += `<p class="prod-bottom__descr prod-chars__item">${key}: ${dataItem.chars[key]}</p>`;
+            });
+
+            prodModalChars.innerHTML = charsItems;
+
+            if (dataItem.video) {
+              prodModalVideo.style.display = "block";
+              prodModalVideo.innerHTML = `
+             <iframe
+              src="${dataItem.video}"
+                allow="accelerometer; autoplay; clipboard-w rite; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+             ></iframe>
+            `;
+            } else {
+              prodModalVideo.style.display = "none";
+            }
           }
         }
       });
